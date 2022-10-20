e7c9709b6704a2fe807a595d8e46d1e1aff7b204
Ziooong <oct_12@naver.com>
2022-10-20 11:14:10 +0900

    templates dirs 수정

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], #템플릿 위치로 따로 지정하고 싶을 때
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ], #템플릿 위치로 따로 지정하고 싶을 때
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
